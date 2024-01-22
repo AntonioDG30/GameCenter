ce136b58146d1473386ed7963dfc30559b697a8f
AntonioDG30 <a.digiorgio8@studenti.unisa.it>
2024-01-23 00:05:30 +0100

    bug

--- src/main/java/control/AdminProductControl.java
+++ src/main/java/control/AdminProductControl.java
@@ -1,26 +1,26 @@
 package control;
 
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.MultipartConfig;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.Part;
-import model.ProductBean;
-import model.ProductModel;
+import model.*;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import javax.servlet.annotation.MultipartConfig;
+
 @MultipartConfig
-@WebServlet("/AdminProductControl")
 public class AdminProductControl extends HttpServlet 
 {
 	
